e2acec878482a304cdec71a2b4aa6e2604320e84
victorFlorentino12 <140027327+victorFlorentino12@users.noreply.github.com>
2023-11-28 20:03:03 -0300

    repair bug

--- src/components/section/Presentation.js
+++ src/components/section/Presentation.js
@@ -39,7 +39,7 @@ function Presentation(){
     return(
         <div id='presetation' className={styles.presentation}>
             <h3><strong>Bem-vindo ao meu Portfólio</strong></h3>
-            <h1>Olá, eu sou {text}</h1>
+            <div className={styles.div_presentation}><h1>Olá, eu sou {text}</h1></div>
             <p> Olá, meu nome é Victor Florentino e sou um programador front-end.  Tenho experiência em desenvolvimento web utilizando HTML, CSS, JavaScript, React.js e Vue.js. Sou apaixonado por criar interfaces intuitivas e atraentes, buscando sempre proporcionar a melhor experiência para os usuários.Ao longo da minha carreira, tenho trabalhado em projetos desafiadores, aplicando as melhores práticas e acompanhando as últimas tendências do mercado. Estou sempre em busca de novos conhecimentos e aprendizados para  aprimorar minhas habilidades. Se você está procurando um profissional dedicado para o seu  projeto, estou aqui para ajudar. Vamos criar algo incrível juntos!</p>
             <ButtonA text='Conecte-se comigo!!' link="https://www.linkedin.com/in/victor-florentino-06a462196/"/>
            
